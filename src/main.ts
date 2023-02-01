41147ca0f07c3eac4ef1016b7eee719797955d5d
Satsuki Akiba <anosatsuk124@gmail.com>
2023-02-02 00:36:24 +0900

    FIX

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { Client, ClientOptions, GatewayIntentBits, Partials } from 'discord.js';
-import { joinVoiceChannel } from '@discordjs/voice';
+import {
+    VoiceConnection,
+    getVoiceConnection,
+    joinVoiceChannel,
+} from '@discordjs/voice';
 import { createWriteStream, writeFile } from 'fs';
 import { opus } from 'prism-media';
 import pathToFfmpeg from 'ffmpeg-static';
 import { exec } from 'child_process';
 import { v4 as uuidv4 } from 'uuid';
 import { config } from 'dotenv';
+import path from 'path';
 config();
 
 if (!process.argv[1]) {
@@ -13,13 +18,12 @@ if (!process.argv[1]) {
     process.exit(1);
 }
 
-const path = process.argv[1];
+const dirPath = process.argv[2];
 
 interface Talk {
-    [key: string]: {
-        data: Date;
-        id: string;
-    };
+    data: Date;
+    discord_id: string;
+    uuid: string;
 }
 
 const options: ClientOptions = {
@@ -37,11 +41,11 @@ client.on('ready', () => {
     console.log(`${client.user?.tag} is logged in`);
 });
 
+const talks: Talk[] = [];
 client.on('messageCreate', (msg) => {
     const idDateMap = new Map<string, Date>();
     if (msg.author.bot) return;
     if (msg.content.startsWith('!join')) {
-        const talks: Talk[] = [];
         const channel = msg.member?.voice.channel;
         const connection = joinVoiceChannel({
             channelId: channel?.id!,
@@ -52,17 +56,15 @@ client.on('messageCreate', (msg) => {
         connection.dispatchAudio();
         const receiver = connection.receiver;
 
-        receiver.speaking.addListener('start', async (userId) => {
+        receiver.speaking.on('start', async (userId) => {
             console.log('start');
-            console.log(userId);
             const stream = receiver.subscribe(userId);
             const uuid = uuidv4();
-            const fileName = `${path}/${uuid}`;
+            const fileName = path.join(dirPath, uuid);
             const talk: Talk = {
-                uuid: {
-                    data: new Date(),
-                    id: userId,
-                },
+                data: new Date(),
+                discord_id: userId,
+                uuid: uuid,
             };
             const pcmFile = createWriteStream(`${fileName}.pcm`);
             stream
@@ -74,7 +76,7 @@ client.on('messageCreate', (msg) => {
                     })
                 )
                 .pipe(pcmFile);
-            receiver.speaking.addListener('end', (endUserId) => {
+            receiver.speaking.on('end', async (endUserId) => {
                 if (userId !== endUserId) return;
                 exec(
                     `${pathToFfmpeg} -y -f s16le -ar 44.1k -ac 2 -i "${fileName}.pcm" "${fileName}.mp3"`,
@@ -84,20 +86,26 @@ client.on('messageCreate', (msg) => {
                         }
                     }
                 );
-                exec(`rm "${fileName}.pcm"`);
                 talks.push(talk);
+                console.log(talks);
                 console.log('end');
             });
         });
+    }
 
-        if (msg.content.startsWith('!leave')) {
-            connection.disconnect();
-            writeFile(`${path}/talks.json`, JSON.stringify(talks), (err) => {
+    if (msg.content.startsWith('!leave')) {
+        const connection = getVoiceConnection(msg.guildId!);
+        if (!connection) return;
+        connection.disconnect();
+        writeFile(
+            path.join(dirPath, 'talks.json'),
+            JSON.stringify(talks),
+            (err) => {
                 if (err) {
                     console.log(err);
                 }
-            });
-        }
+            }
+        );
     }
 });
 
